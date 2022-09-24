11023418ad7c6756c0e1ae47e37f7c3cb2070157
Chris Maki <c.william.developer@gmail.com>
2022-09-23 21:44:12 -0500

    Input entered, then click add employee, working correctly. It adds to the state array

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-$(document).ready(onReady)
+$(document).ready(onReady);
+let employeeArr = [];
 
 function onReady() {
-   
-    console.log('Good to go!');
-}
+   $('#addData').on('click',addPerson);//Call a function when clicked.
+
+ }
+
+ function addPerson(evt) {
+    evt.preventDefault();
+
+    let newObj = {
+        fName: $('#fName').val(),
+        lName: $('#lName').val(),
+        ID: $('#ID').val(),
+        title: $('#Title').val(),
+        salary: $('#Salary').val()
+    }
+    
+    employeeArr.push(newObj);
+    console.log(newObj);
+ }
