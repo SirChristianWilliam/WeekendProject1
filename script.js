7c25347323207aac8753290dc07c2d00892e4e8d
Chris Maki <c.william.developer@gmail.com>
2022-09-24 21:13:58 -0500

    Doing some rearranging of my code, because I can't get the stretch goals to work

--- script.js
+++ script.js
@@ -17,25 +17,32 @@ function onReady() {
         title: $('#Title').val(),
         salary: $('#Salary').val()
     }
-    
+   
     employeeArr.push(newObj); //add the object to the state array
- 
-    $('#theTable').append(` // add the information to the DOM
+
+    appendEmployee(newObj);
+}
+function appendEmployee(arr) {
+    $('#tableRowTD').empty();
+
+    employeeArr.push(arr);
+// add the information to the DOM
+     $('#theTable').append(` 
     <tr>
         <td>
-        ${newObj.fName}
+        ${arr.fName}
         </td> 
         <td>
-        ${newObj.lName}
+        ${arr.lName}
         </td>
         <td>
-        ${newObj.ID}
+        ${arr.ID}
         </td> 
         <td>
-        ${newObj.title}
+        ${arr.title}
         </td>
-        <td>
-        ${newObj.salary}
+        <td class="celery">
+        ${arr.salary}
         </td>
         <td>
         <button class="deleteEmp">Del</button>
@@ -50,23 +57,29 @@ addTotal();
     $('#ID').val('');
     $('#Title').val('');
     $('#Salary').val('');
-   }
+ }
 
  function addTotal() { 
     total=0;//Reset the total
+
       for(let sal of employeeArr) {//Loop through entire array
         total+= Number(sal.salary);//Find all the current array's salary values
        }
        
        $('#total').text(Number(total/12)); //Add the total salary values together & display on the DOM
+       $('#total').empty();
+       $('#total').append('<h3>Total Monthly: $' + total.toFixed(2) +  '<h3>'); 
        if($('#total').text() >= 20000) {
         $('#total').addClass('tooMuch') //add a css class if the value is as specified
        }
        
   }
+   function onDelete(evt) {
+    evt.preventDefault();
+     
+     $('#total').text($('#total').text());
+      console.log($(this).parent().text());  
+    $(this).parent().parent().remove();
 
-  function onDelete() {
-     $(this).parent().parent().remove();
     }
-
 
